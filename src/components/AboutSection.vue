8bf9b8694a464b8bff466a3e490189fcd339aef7
Jaymi Yam <jaymiyam@gmail.com>
2026-05-24 18:28:36 +0800

    added playground project

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -23,7 +23,7 @@
         </p>
         <p class="text-balance">
           currently learning:
-          <span class="text-orangeGold font-bold">graphQL</span>
+          <span class="text-orangeGold font-bold">D3.js</span>
         </p>
       </div>
       <div>
